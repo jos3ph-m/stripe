48326653beee2e6ef15b63dca7f0b8fcdc24c85e
Joseph M <jmedina1985@hotmail.com>
2021-03-25 19:55:26 -0700

    Setting up context

--- src/context.js
+++ src/context.js
@@ -1,2 +1,11 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  return <AppContext.Provider>{children}</AppContext.Provider>;
+};
