bec8390d9330a18ede5995ad5d446f34d1ea244c
Joseph M <jmedina1985@hotmail.com>
2021-03-26 19:26:40 -0700

    Working on the navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -17,10 +17,15 @@ const Navbar = () => {
         <ul className="nav-links">
           <li>
             <button className="link-btn">products</button>
+          </li>
+          <li>
             <button className="link-btn">developers</button>
+          </li>
+          <li>
             <button className="link-btn">company</button>
           </li>
         </ul>
+        <button className="btn signin-btn">Sign In</button>
       </div>
     </nav>
   );
