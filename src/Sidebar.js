5d0441790310bbd5416a77fc4e6638905b52e3c7
Joseph M <jmedina1985@hotmail.com>
2021-03-26 20:17:52 -0700

    Working with the sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import sublinks from './data';
+import { useGlobalContext } from './context';
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
-}
+  return <h2>sidebar component</h2>;
+};
 
-export default Sidebar
+export default Sidebar;
