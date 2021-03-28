6ed527f848cb536a8c130cbb960bb41a492da8e2
Joseph M <jmedina1985@hotmail.com>
2021-03-27 21:25:40 -0700

    working on submenu

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,7 @@ import { useGlobalContext } from './context';
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {};
   return (
     <nav className="nav">
       <div className="nav-center">
