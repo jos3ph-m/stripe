50e1e2a44b9c7ca3dc292dc16cb86be53d05b267
Joseph M <jmedina1985@hotmail.com>
2021-03-27 21:19:17 -0700

    working on submenu

--- src/Submenu.js
+++ src/Submenu.js
@@ -3,7 +3,11 @@ import { useGlobalContext } from './context';
 
 const Submenu = () => {
   const { isSubmenuOpen } = useGlobalContext();
-  return <h2>submenu component</h2>;
+  return (
+    <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}>
+      submenu
+    </aside>
+  );
 };
 
 export default Submenu;
