2145c26e6f66ef7fdaf66c139efa1a95b0ad645a
Joseph M <jmedina1985@hotmail.com>
2021-03-27 21:18:09 -0700

    working on submenu

--- src/Submenu.js
+++ src/Submenu.js
@@ -1,7 +1,8 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import { useGlobalContext } from './context';
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  return <h2>submenu component</h2>;
+};
 
-export default Submenu
+export default Submenu;
