6c81562cd50eb9521c6c7e40b790b398eb03ea33
Joseph M <jmedina1985@hotmail.com>
2021-03-28 20:07:35 -0700

    dealing with submenu links

--- src/Submenu.js
+++ src/Submenu.js
@@ -7,12 +7,23 @@ const Submenu = () => {
     location,
     page: { page, links },
   } = useGlobalContext();
+
   const container = useRef(null);
+  const [columns, setColumns] = useState('col-2');
+
   useEffect(() => {
+    setColumns('col-2');
     const submenu = container.current;
     const { center, bottom } = location;
     submenu.style.left = `${center}px`;
     submenu.style.top = `${bottom}px`;
+    if (links.length === 3) {
+      setColumns('col-3');
+    }
+
+    if (links.length > 3) {
+      setColumns('col-4');
+    }
   }, [location]);
   return (
     <aside
@@ -20,7 +31,7 @@ const Submenu = () => {
       ref={container}
     >
       <h4>{page}</h4>
-      <div className={`submenu-center col-2`}>
+      <div className={`submenu-center ${columns}`}>
         {links.map((link, index) => {
           const { label, icon, url } = link;
           return (
